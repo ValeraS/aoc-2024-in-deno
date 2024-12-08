6feed7b24ca67381c3671183bca5fd33a4327a21
Valerii Sidorenko <balepas@nebius.com>
2024-12-08 06:36:25 +0100

    eighth task

---
+++ 08/8_1.ts
@@ -0,0 +1,45 @@
+function countAntidote(puzzle: string[]) {
+  const positions = new Map<string, [number, number][]>();
+  const antidotes = new Set<string>();
+  for (let i = 0; i < puzzle.length; i++) {
+    for (let j = 0; j < puzzle[i].length; j++) {
+      const freq = puzzle[i][j];
+      if (freq === ".") {
+        continue;
+      }
+
+      const freqPositions = positions.get(freq) ?? [];
+      for (const p of freqPositions) {
+        const difX = i - p[0];
+        const difY = j - p[1];
+
+        let antidoteX = p[0] - difX;
+        let antidoteY = p[1] - difY;
+        if (
+          antidoteX >= 0 && antidoteX < puzzle.length && antidoteY >= 0 &&
+          antidoteY < puzzle[i].length
+        ) {
+          antidotes.add(`${antidoteX}:${antidoteY}`);
+        }
+
+        antidoteX = i + difX;
+        antidoteY = j + difY;
+        if (
+          antidoteX >= 0 && antidoteX < puzzle.length && antidoteY >= 0 &&
+          antidoteY < puzzle[i].length
+        ) {
+          antidotes.add(`${antidoteX}:${antidoteY}`);
+        }
+      }
+      freqPositions.push([i, j]);
+      positions.set(freq, freqPositions);
+    }
+  }
+  return antidotes.size;
+}
+
+if (import.meta.main) {
+  const filename = Deno.args[0];
+  const puzzle = (await Deno.readTextFile(filename)).split("\n").slice(0, -1);
+  console.log(countAntidote(puzzle));
+}
